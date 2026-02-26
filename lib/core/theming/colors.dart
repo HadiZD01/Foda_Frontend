e5cec0844310718e3ce5f51e77574a45a4486c38
HadiZD01 <zidanihadi259@gmail.com>
2026-02-26 21:10:12 +0100

    -setup code -Colors -Styles -packeages

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static const Color mainblue = Color(0xFF247CFF);
+  static const Color DarkBlue = Color(0xFF242424);
+  static const Color morelighgray = Color(0xFFFDFDFF);
+  static const Color gray = Color(0xFF757575);
+  static const Color LigherGray = Color(0xFFEDEDED);
+  static const Color lighgray = Color(0xFFC2C2C2);
+  static const Color morelightergray = Color(0xFFF5F5F5);
+}
